2581d9ccb19f840571a6e5aec1b9411e56664bbe
LukeBaum <luke@trimd.com>
2025-12-04 22:41:04 -0600

    Added day 4

---
+++ AOC-2025-4-1/Program.vb
@@ -0,0 +1,108 @@
+Imports System
+Imports System.IO
+Imports System.Drawing
+Imports System.Runtime.CompilerServices
+
+Module Program
+    Sub Main(args As String())
+
+        ' Validate the correct number of arguments are present
+        If args.Length = 0 Then Console.WriteLine("No rolls file provided")
+        If args.Length > 1 Then Console.WriteLine("Too many arguments")
+        If args.Length <> 1 Then Exit Sub
+
+        ' Make sure the path to the rolls file exists
+        Dim RollsFilePath As String = args(0)
+        If Not File.Exists(RollsFilePath) Then
+            Console.WriteLine($"Rolls file doesn't exist: {RollsFilePath}")
+            Exit Sub
+        End If
+
+        ' Make sure we can actually read the file
+        Dim RollsStrings() As String = Nothing
+        Try
+            RollsStrings = File.ReadAllLines(RollsFilePath)
+        Catch ex As Exception
+            Console.WriteLine($"Error reading rolls file: {ex.Message}")
+            Exit Sub
+        End Try
+
+        ' Make sure the file wasn't empty
+        If RollsStrings.Count < 1 Then
+            Console.WriteLine("No rolls in the file")
+            Exit Sub
+        End If
+
+        ' Parse the file
+        Dim PaperSlots As New List(Of PaperSlot)
+        PaperSlots.ImportSlotsFromStrings(RollsStrings)
+
+        ' Do the math
+        ' This is SO MUCH SLOWER than using a multidimensional array, but it's funny
+        Dim AccessibleRolls As Integer = 0
+        For Y As Integer = 0 To PaperSlots.Count - 1
+            For X As Integer = 0 To RollsStrings(0).Length - 1
+                If PaperSlots.SlotContainsAccessiblePaperRoll(X, Y) Then AccessibleRolls += 1
+            Next
+        Next
+        Console.WriteLine($"Accessible roll count: {CStr(AccessibleRolls)}")
+
+    End Sub
+
+    Public Class PaperSlot
+
+        Public Property Occupied As Boolean = False
+        Public Property Location As Point = New Point(-1, -1)
+
+        Sub New(Optional ByVal IsOccupied As Boolean = False, Optional ByVal X As Integer = -1, Optional ByVal Y As Integer = -1)
+            Me.Occupied = IsOccupied
+            Me.Location = New Point(X, Y)
+        End Sub
+
+    End Class
+
+    <Extension()> Public Sub ImportSlotsFromStrings(ByRef PaperSlots As List(Of PaperSlot), ByRef RollsStrings() As String)
+
+        For Y As Integer = 0 To RollsStrings.Length - 1
+            Dim CurrentRow As String = RollsStrings(Y)
+            For X As Integer = 0 To CurrentRow.Length - 1
+                PaperSlots.Add(New PaperSlot(CurrentRow.Substring(X, 1) = "@", X, Y))
+            Next
+        Next
+
+    End Sub
+
+    <Extension()> Public Function GetSlotAtLocation(ByRef PaperSlots As List(Of PaperSlot), ByVal X As Integer, ByVal Y As Integer) As PaperSlot
+        For Each Slot As PaperSlot In PaperSlots
+            If Slot.Location.X = X And Slot.Location.Y = Y Then
+                Return Slot
+                Exit For
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    <Extension()> Public Function SlotContainsAccessiblePaperRoll(ByRef PaperSlots As List(Of PaperSlot), ByVal X As Integer, ByVal Y As Integer) As Boolean
+
+        Dim SelectedPaperSlot As PaperSlot = PaperSlots.GetSlotAtLocation(X, Y)
+
+        If SelectedPaperSlot Is Nothing Then Return False
+        If SelectedPaperSlot.Occupied = False Then Return False
+
+        Dim AdjacentRolls As Integer = 0
+
+        For a As Integer = -1 To 1
+            For b As Integer = -1 To 1
+                If a = 0 And b = 0 Then Continue For
+                Dim AdjacentSlot As PaperSlot = PaperSlots.GetSlotAtLocation(X + a, Y + b)
+                If AdjacentSlot Is Nothing Then Continue For
+                If AdjacentSlot.Occupied Then AdjacentRolls += 1
+                If AdjacentRolls >= 4 Then Return False
+            Next
+        Next
+
+        Return True
+
+    End Function
+
+End Module
